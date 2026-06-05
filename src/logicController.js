dc09a570980d775058ba36c9a549f34a022e8903
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2026-06-05 20:00:55 +0300

    feat: implement getProject, deleteProject and editProject functionality. Move UUID generation to logic instead of UI.

--- src/logicController.js
+++ src/logicController.js
@@ -1,10 +1,10 @@
 import { saveToStorage, loadFromStorage } from './store.js';
 import Project from './classes/Projects.js';
 
-export const addProject = (formData) => {
-	const { id, title } = formData;
+export const addProject = (projectTitle) => {
+	const id = crypto.randomUUID();
 	const projects = loadFromStorage();
-	const newProject = new Project(id, title);
+	const newProject = new Project(id, projectTitle);
 
 	projects.push(newProject);
 	saveToStorage(projects);
@@ -17,5 +17,24 @@ export const getAllProjects = () => {
 	);
 
 	return rehydratedProjects;
+};
+
+export const getProject = (projectId) => {
+	const projects = getAllProjects();
+	return projects.find((project) => project.id === projectId);
+};
+
+export const deleteProject = (projectId) => {
+	const projects = loadFromStorage();
+	const projectsToKeep = projects.filter((project) => project.id !== projectId);
+	saveToStorage(projectsToKeep);
+};
+
+export const editProject = (editFormData) => {
+	const projects = getAllProjects();
+	const { id, title } = editFormData;
+	const projectToEdit = projects.find((project) => project.id === id);
+	projectToEdit.updateFields(editFormData);
+	saveToStorage(projects);
 };
 
