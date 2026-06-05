cec2a22bd76af4cd2157557f5d3d003253f5389b
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2026-06-05 20:26:40 +0300

    feat: implement project UI - render, modal and edit/delete.

--- src/uiController.js
+++ src/uiController.js
@@ -1,10 +1,17 @@
-import { addProject, getAllProjects } from './logicController.js';
+import {
+	addProject,
+	deleteProject,
+	editProject,
+	getAllProjects,
+} from './logicController.js';
 
 const elements = {
-	projetModal: document.querySelector('#project-modal'),
+	openModalBtn: document.querySelector('#open-project-modal'),
+	projectModal: document.querySelector('#project-modal'),
 	projectForm: document.querySelector('.project-form'),
 	projectFormTitleInput: document.querySelector('#project-title'),
 	projectList: document.querySelector('.projects'),
+	projectFormBtn: document.querySelector("button[type='submit']"),
 };
 
 const clearProjectsContainer = () => {
@@ -14,31 +21,86 @@ const clearProjectsContainer = () => {
 	});
 };
 
-const renderProjects = () => {
-	const projects = getAllProjects();
-	clearProjectsContainer();
+const createProjectElement = (project) => {
+	const listEl = document.createElement('li');
+	listEl.textContent = project.title;
+	const editBtn = document.createElement('button');
+	const delBtn = document.createElement('button');
+	editBtn.textContent = 'Edit';
+	editBtn.dataset.buttonType = 'edit';
+	delBtn.textContent = 'Delete';
+	editBtn.addEventListener('click', (e) => handleProjectEdit(e, project));
+	delBtn.addEventListener('click', () => handleDeleteProject(project.id));
+	listEl.appendChild(editBtn);
+	listEl.appendChild(delBtn);
+	return listEl;
+};
+
+const createProjectList = (projects) => {
 	projects.forEach((project) => {
-		const listEl = document.createElement('li');
-		listEl.textContent = `${project.id.slice(0, 5)}... / ${project.title}`;
-		elements.projectList.appendChild(listEl);
+		const projectListEntry = createProjectElement(project);
+		elements.projectList.appendChild(projectListEntry);
 	});
 };
 
-const handleAddProject = (e) => {
-	e.preventDefault();
-	const formData = {
-		id: crypto.randomUUID(),
-		title: elements.projectFormTitleInput.value,
-	};
-	addProject(formData);
-	renderProjects();
+const renderProjects = () => {
+	const projects = getAllProjects();
+	clearProjectsContainer();
+	createProjectList(projects);
+};
+
+const setProjectEditFields = (project) => {
+	elements.projectFormTitleInput.value = project.title;
+	elements.projectForm.dataset.projectId = project.id;
+};
+
+const setProjectModalType = (e) => {
+	const { buttonType } = e.currentTarget.dataset;
+	elements.projectFormBtn.textContent = `${buttonType[0]
+		.toUpperCase()
+		.concat(buttonType.slice(1))} Project`;
+	elements.projectModal.dataset.type = buttonType;
 	elements.projectForm.reset();
-	elements.projetModal.close();
+};
+
+const openEditModal = () => {
+	elements.projectModal.showModal();
+};
+
+const handleProjectEdit = (e, project) => {
+	setProjectModalType(e);
+	openEditModal();
+	setProjectEditFields(project);
+};
+
+const handleFormSubmit = (e) => {
+	e.preventDefault();
+
+	if (elements.projectModal.dataset.type === 'add') {
+		const projectTitle = elements.projectFormTitleInput.value;
+		addProject(projectTitle);
+	}
+
+	if (elements.projectModal.dataset.type === 'edit') {
+		const projectFormData = {
+			id: elements.projectForm.dataset.projectId,
+			title: elements.projectFormTitleInput.value,
+		};
+		editProject(projectFormData);
+	}
+
+	elements.projectModal.close();
+	renderProjects();
+};
+
+const handleDeleteProject = (projectId) => {
+	deleteProject(projectId);
+	renderProjects();
 };
 
 const registerEventListeners = () => {
-	elements.projectForm.addEventListener('submit', handleAddProject);
+	elements.projectForm.addEventListener('submit', handleFormSubmit);
+	elements.openModalBtn.addEventListener('click', setProjectModalType);
 };
 
 export { renderProjects, registerEventListeners };
-
