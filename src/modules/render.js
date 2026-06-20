e1df9883646a4bbb2ebffdf9579e9c5b5672b6fa
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2026-06-20 18:09:05 +0300

    refactor: move render logic into its own module.

---
+++ src/modules/render.js
@@ -0,0 +1,96 @@
+import { createElement, appendChildern } from './../utils/dom.js';
+import { formatDate } from './../utils/date.js';
+import editIcon from './../assets/edit.svg';
+import deleteIcon from './../assets/delete.svg';
+import calendar from './../assets/calendar.svg';
+
+const clearContainer = (parent) => {
+	const children = [...parent.childNodes];
+	children.forEach((child) => {
+		parent.removeChild(child);
+	});
+};
+
+const createTodoElement = (todo) => {
+	const listEl = createElement('li', { className: 'todo' });
+	const divLeft = createElement('div', { className: 'left' });
+	const divRight = createElement('div', { className: 'right' });
+	const todoTitle = createElement('h4', { textContent: todo.title });
+	const dateEl = createElement('p', {
+		className: 'todo-date',
+		innerHTML: calendar,
+	});
+	const formattedDate = formatDate(todo.date);
+	const dateText = document.createTextNode(formattedDate);
+	dateEl.appendChild(dateText);
+	const editBtn = createElement('button', {
+		className: 'action-btn',
+		innerHTML: editIcon,
+	});
+	const delBtn = createElement('button', {
+		className: 'action-btn',
+		innerHTML: deleteIcon,
+	});
+
+	appendChildern(divLeft, [todoTitle, dateEl]);
+	appendChildern(divRight, [editBtn, delBtn]);
+	appendChildern(listEl, [divLeft, divRight]);
+	return listEl;
+};
+
+const createTodoList = (todos, { doneList, notDoneList }) => {
+	todos.forEach((todo) => {
+		const todoListEntry = createTodoElement(todo);
+
+		if (todo.status === 'true') {
+			doneList.appendChild(todoListEntry);
+		} else {
+			notDoneList.appendChild(todoListEntry);
+		}
+	});
+};
+
+const renderTodos = (todos, activeProjectId, { doneList, notDoneList }) => {
+	clearContainer(doneList);
+	clearContainer(notDoneList);
+
+	if (todos.length !== 0) {
+		createTodoList(todos, { doneList, notDoneList });
+	}
+};
+
+const createProjectElement = (project, activeProjectId) => {
+	const listEl = createElement('li');
+	listEl.dataset.id = project.id;
+	if (project.id === activeProjectId) {
+		listEl.classList.add('selected');
+	}
+	const projectTitle = createElement('span', { textContent: project.title });
+	const btnContainer = createElement('div', { className: 'controls' });
+	const editBtn = createElement('button', {
+		className: 'action-btn',
+		innerHTML: editIcon,
+	});
+	const delBtn = createElement('button', {
+		className: 'action-btn',
+		innerHTML: deleteIcon,
+	});
+	appendChildern(btnContainer, [editBtn, delBtn]);
+	appendChildern(listEl, [projectTitle, btnContainer]);
+	return listEl;
+};
+
+const createProjectList = (projects, container, activeProjectId) => {
+	projects.forEach((project) => {
+		const projectListEntry = createProjectElement(project, activeProjectId);
+		container.appendChild(projectListEntry);
+	});
+};
+
+const renderProjects = (projects, container, activeProjectId) => {
+	clearContainer(container);
+	createProjectList(projects, container, activeProjectId);
+};
+
+export { renderProjects, renderTodos };
+
