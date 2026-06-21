662588ac37cea50095c0eef5eb1adb37b799dd7b
Aleksandr <113410543+hammerztein@users.noreply.github.com>
2026-06-21 14:04:52 +0300

    refactor: set in-memory array to reducde function calls to the localStorage.

--- src/modules/logicController.js
+++ src/modules/logicController.js
@@ -1,82 +1,76 @@
 import { saveToStorage, loadFromStorage } from './store.js';
 import Project from '../classes/Projects.js';
 
-export const addProject = (projectTitle) => {
-	const id = crypto.randomUUID();
-	const projects = loadFromStorage();
-	const newProject = new Project(id, projectTitle);
-	projects.push(newProject);
+const projects = loadFromStorage().map(Project.fromJSON);
+
+const saveProjects = () => {
 	saveToStorage(projects);
+};
+
+export const createDefaultProject = () => {
+	const id = crypto.randomUUID();
+	const defaultProject = new Project(id, 'Default Project');
+	projects.push(defaultProject);
+	saveProjects();
 };
 
 export const getAllProjects = () => {
-	const projects = loadFromStorage();
-	const rehydratedProjects = projects.map((project) =>
-		Project.fromJSON(project),
-	);
-
-	return rehydratedProjects;
+	return [...projects];
 };
 
 export const getProject = (projectId) => {
-	const projects = getAllProjects();
 	return projects.find((project) => project.id === projectId);
 };
 
+export const addProject = (projectTitle) => {
+	const id = crypto.randomUUID();
+	const newProject = new Project(id, projectTitle);
+	projects.push(newProject);
+	saveProjects();
+};
+
 export const deleteProject = (projectId) => {
-	const projects = loadFromStorage();
-	const projectsToKeep = projects.filter((project) => project.id !== projectId);
-	saveToStorage(projectsToKeep);
+	const projectsIndex = projects.findIndex(
+		(project) => project.id === projectId,
+	);
+	projects.splice(projectsIndex, 1);
+	saveProjects();
 };
 
 export const editProject = (editFormData) => {
-	const projects = getAllProjects();
-	const { id, title } = editFormData;
-	const projectToEdit = projects.find((project) => project.id === id);
+	const projectToEdit = getProject(editFormData.id);
 	projectToEdit.updateFields(editFormData);
-	saveToStorage(projects);
+	saveProjects();
 };
 
-export const addTodo = (projectId, todoObj) => {
-	const projects = getAllProjects();
-	const projectToAddTodo = projects.find((project) => project.id === projectId);
-	todoObj.id = crypto.randomUUID();
-	projectToAddTodo.addTodo(todoObj);
-	saveToStorage(projects);
+export const getTodo = (projectId, todoId) => {
+	return getProject(projectId).getTodo(todoId);
 };
 
 export const getTodos = (projectId) => {
-	const project = getProject(projectId);
-	return project.todos;
+	return getProject(projectId).getTodos();
+};
+
+export const addTodo = (projectId, todoObj) => {
+	todoObj.id = crypto.randomUUID();
+	getProject(projectId).addTodo(todoObj);
+	saveProjects();
 };
 
 export const getTodosStatusCount = (projectId) => {
 	const todos = getTodos(projectId);
-	const doneTodoCount = todos.filter((todo) => todo.status === 'true').length;
-	const notDoneTodoCount = todos.filter(
-		(todo) => todo.status === 'false',
-	).length;
 	return {
-		done: doneTodoCount,
-		notDone: notDoneTodoCount,
+		done: todos.filter((todo) => todo.status === 'true').length,
+		notDone: todos.filter((todo) => todo.status === 'false').length,
 	};
 };
 
 export const editTodo = (projectId, editFormData) => {
-	const projects = getAllProjects();
-	const projectToUpdateTodo = projects.find(
-		(project) => project.id === projectId,
-	);
-	projectToUpdateTodo.editTodo(editFormData);
-
-	saveToStorage(projects);
+	getProject(projectId).editTodo(editFormData);
+	saveProjects();
 };
 
 export const deleteTodo = (projectId, todoId) => {
-	const projects = getAllProjects();
-	const projectToDeleteTodo = projects.find(
-		(project) => project.id === projectId,
-	);
-	projectToDeleteTodo.removeTodo(todoId);
-	saveToStorage(projects);
+	getProject(projectId).removeTodo(todoId);
+	saveProjects();
 };
